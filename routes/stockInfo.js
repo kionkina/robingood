d0b88c65ea16e78081d37690088450645bc10b96
Karina Ionkina <karina.ionkina15@gmail.com>
2020-04-28 16:25:47 -0400

    add hotStock endpoint

--- routes/stockInfo.js
+++ routes/stockInfo.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const axios = require("axios");
+const cheerio = require("cheerio");
+
+
 
 //Gets list of tickers and stock names based on key
 router.get('/search/:key', (req, res, next) => {
@@ -76,6 +79,61 @@ router.get('/news/:ticker', (req, res, next) => {
         console.log(err)
         );
 });
+
+
+//returns list of top movers from 
+//https://www.etrade.wallst.com/Research/Markets/Movers
+router.get('/hotStocks', (req, res, next) => {
+    fetchData();
+});
+
+
+    const fetchData = async () => {
+    axios.get('https://www.etrade.wallst.com/Research/Markets/Movers')
+   .then((response) => {
+       if(response.status === 200) {
+           const html = response.data;
+           const $ = cheerio.load(html); 
+           let topMovers = [];
+           //console.log($);
+           $('.col-2').each(function(i, elem) {
+                topMovers.push($($(elem).children()[1]).text());
+               });   
+               topMovers = topMovers.slice(1);
+               console.log(topMovers);
+               return topMovers; 
+            }
+           
+}, (error) => console.log(err) );
+
+};
+
+
+    const getResults = async () => {
+        const $ = await fetchData();
+        //console.log($);
+        console.log($);
+        //console.log($(".MarketTop-topTable"));
+        console.log("^");
+        let tickers = [];
+
+        $('.MarketTop-symbol').each(function(i, item){
+            console.log($(this));
+            tickers.push($(this).find('a').text());
+    });
+        console.log(tickers);
+        /*.each((item, index) => 
+        {
+            console.log($(item).text());
+        }
+        );*/
+
+
+    
+
+    }
+
+
 
 
 module.exports = router;
