c24722159a641391ead4722105a9570ace6f16f3
Peter Lee <peterdjlee@gmail.com>
2020-05-03 16:26:54 -0400

    implement endpoint that returns updated portfolio value

--- routes/user.js
+++ routes/user.js
@@ -29,4 +29,45 @@ router.get('/:userId', (req, res, next) => {
         });
 });
 
+
+// Gets the newest portfolio value.
+router.get('/portfolioValue/:userId', (req, res, next) => {
+    const userId = req.params.userId;
+    User.findById(userId).exec()
+        .then(doc => {
+
+            // If the document with the given id exists
+            if (doc) {
+                const stocks = doc.stocks;
+                // For each stock, add to portfolio value by calculating current price * quantity.
+                let portfolioValue = 0;
+                stocks.forEach((item, index) => {
+                    console.log(item)
+                    portfolioValue += item.currentPrice * item.quantity
+                });
+                const filter = { _id: userId };
+                const update = { portfolioValue: portfolioValue };
+                
+                // Update portfolio value in the database.
+                User.findOneAndUpdate(filter, update, { new: true, upsert: true });
+
+                var dict = {};
+                dict['userId'] = userId;
+                dict['portfolioValue'] = portfolioValue;
+
+                res.status(200).json(dict);
+            } else {
+                res.status(404).json({
+                    message: 'No valid entry found for provided userId'
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
 module.exports = router;
